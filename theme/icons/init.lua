e20b0f5f521dd85fdaca46614a27dfe047fac4c8
MusashiDaryl <73018127+MusashiDaryl@users.noreply.github.com>
2020-10-30 20:10:14 +0000

    Add files via upload

---
+++ theme/icons/init.lua
@@ -0,0 +1,39 @@
+local dir = os.getenv('HOME') .. '/.config/awesome/theme/icons'
+
+return {
+  --tags
+  brave = dir .. '/brave.svg',
+  vivaldi = dir .. '/vivaldi.svg',
+  firefox = dir .. '/firefox.svg',
+  chrome = dir .. '/google-chrome.svg',
+  mail = dir .. '/mail.svg',
+  trading = dir .. '/trading.svg',
+  vm = dir .. '/vm.svg',
+  pdf = dir .. '/pdf.svg',
+  kodi = dir .. '/kodi.svg',
+  slack = dir .. '/slack.svg',
+  discord = dir .. '/discord.svg',
+  steam = dir .. '/steam.svg',
+  code = dir .. '/code-braces.svg',
+  social = dir .. '/forum.svg',
+  folder = dir .. '/folder.svg',
+  music = dir .. '/music.svg',
+  game = dir .. '/google-controller.svg',
+  lab = dir .. '/flask.svg',
+  --others
+  menu = dir .. '/menu.svg',
+  close = dir .. '/close.svg',
+  logout = dir .. '/logout.svg',
+  sleep = dir .. '/power-sleep.svg',
+  power = dir .. '/power.svg',
+  lock = dir .. '/lock.svg',
+  restart = dir .. '/restart.svg',
+  search = dir .. '/magnify.svg',
+  volume = dir .. '/volume-high.svg',
+  brightness = dir .. '/brightness-7.svg',
+  chart = dir .. '/chart-areaspline.svg',
+  memory = dir .. '/memory.svg',
+  harddisk = dir .. '/harddisk.svg',
+  thermometer = dir .. '/thermometer.svg',
+  plus = dir .. '/plus.svg'
+}
